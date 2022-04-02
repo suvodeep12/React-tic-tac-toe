7bf58bc5feb82a36de6996e33b2d611f5a68efa8
Suvodeep Ghosh <suvodeepg@gmail.com>
2022-04-02 18:42:23 +0530

    Debug.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
-    const squares = this.state.squares.slice();
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
